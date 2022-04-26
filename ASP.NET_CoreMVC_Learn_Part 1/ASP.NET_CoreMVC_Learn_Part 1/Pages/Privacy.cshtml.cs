aca2ef3993fd50cde214368d837e9888ffc5fc09
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-04-26 15:42:58 +0600

    Created first web asp .net core project for learn part 1

---
+++ ASP.NET_CoreMVC_Learn_Part 1/ASP.NET_CoreMVC_Learn_Part 1/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASP.NET_CoreMVC_Learn_Part_1.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
