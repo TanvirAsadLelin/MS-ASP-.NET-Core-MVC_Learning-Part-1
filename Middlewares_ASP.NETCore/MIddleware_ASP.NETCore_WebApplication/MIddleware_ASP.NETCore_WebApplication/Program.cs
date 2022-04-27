5865a68864290c35e6cccc8ad83ca9e569f6275f
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-04-27 20:31:10 +0600

    Learn Middleware completed with theory

--- Middlewares_ASP.NETCore/MIddleware_ASP.NETCore_WebApplication/MIddleware_ASP.NETCore_WebApplication/Program.cs
+++ Middlewares_ASP.NETCore/MIddleware_ASP.NETCore_WebApplication/MIddleware_ASP.NETCore_WebApplication/Program.cs
@@ -10,6 +10,49 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
 }
+
+app.Map("/map1", mapappbuilder =>
+{
+    mapappbuilder.Run(async context=>
+    {
+        await context.Response.WriteAsync("Map1\n");
+    });
+});
+
+app.UseWhen(context => context.Request.Query.ContainsKey("Branch"),
+    myconappbuilder => {
+
+        myconappbuilder.Use(async (context, next) =>
+       {
+           await context.Response.WriteAsync("Use When\n");
+           await next();
+           await context.Response.WriteAsync("55\n");
+       });
+    });
+
+app.Use(async (context, next) =>
+{
+    await context.Response.WriteAsync("First Middleware\n");
+    await next();
+    await context.Response.WriteAsync("5\n");
+});
+app.Use(async (context, next) =>
+{
+    await context.Response.WriteAsync("Second Middleware\n");
+    await next();
+    await context.Response.WriteAsync("4\n");
+});
+
+app.Run(async context =>
+{
+    await context.Response.WriteAsync("Hello Run mathod\n");
+    await context.Response.WriteAsync("3\n");
+
+});
+
+
+
+
 app.UseStaticFiles();
 
 app.UseRouting();
@@ -19,3 +62,8 @@ app.UseAuthorization();
 app.MapRazorPages();
 
 app.Run();
+
+
+
+
+
