f3877a8a5e16b00443cf5c658a97cf19be96961a
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-04-28 16:35:03 +0600

    Attribute routing completed successfuly

---
+++ Routing_ASP.NETCore/AttributeBasedRouting_WebApplication/AttributeBasedRouting_WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AttributeBasedRouting_WebApplication.Controllers
+{   
+    //[Route("Home")]
+
+    //Token
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {  
+        [Route ("")]
+      //  [Route("Index")]
+      //Token
+        [Route("[action]")]
+        [Route("~/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //For use differnt name of Action method
+
+        //  [Route("About")]
+        //Token
+        [Route("[action]")]
+        public IActionResult About()
+        {
+            //For specify view file name which i want to disply here
+            //return View("~/Views/Home/About.cshtml"); 
+            return View();
+
+        }
+
+
+        [Route("Details/{id}")]  //Id Must be used
+        [Route("Details/{id?}")]  //Id Optional
+        public int Details(int? id)
+        {
+            return id??1;
+        }
+    }
+}
---
+++ Routing_ASP.NETCore/AttributeBasedRouting_WebApplication/AttributeBasedRouting_WebApplication/Program.cs
@@ -0,0 +1,29 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+//app.UseHttpsRedirection();
+//app.UseStaticFiles();
+
+app.UseRouting();
+
+//app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    // To load all attribute routes
+    endpoints.MapControllers();
+});
+
+
+
+
+app.Run();
