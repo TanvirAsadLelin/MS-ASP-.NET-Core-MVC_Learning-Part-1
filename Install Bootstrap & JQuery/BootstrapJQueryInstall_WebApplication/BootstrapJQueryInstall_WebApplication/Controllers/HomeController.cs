39bbcadadfa07acd63caa683322a102a4ecaa73b
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-04-29 22:44:56 +0600

    Installing Bootstrap & JQuery completed successfuly

---
+++ Install Bootstrap & JQuery/BootstrapJQueryInstall_WebApplication/BootstrapJQueryInstall_WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BootstrapJQueryInstall_WebApplication.Controllers
+{   
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {   
+        [Route("")]
+        [Route("~/")]
+        [Route("[action]")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("[action]")]
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
