8af31e001ac1c335ceea45a10abfffef3e2bffd2
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-04-30 14:29:18 +0600

    View & Razor concept theory with example

---
+++ View, RazorSyntax,RazorViewEngine/RazorSyntax_Demo_WebApplication/RazorSyntax_Demo_WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RazorSyntax_Demo_WebApplication.Controllers
+{  
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {    
+        [Route("")]
+        [Route("~/")]
+        [Route("[action]")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
