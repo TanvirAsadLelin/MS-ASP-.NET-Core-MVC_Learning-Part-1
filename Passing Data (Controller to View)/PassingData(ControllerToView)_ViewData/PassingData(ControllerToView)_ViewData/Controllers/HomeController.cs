b86abae2be2d356f3be570a06da2282550269b87
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-04-30 20:02:29 +0600

    View & Razor concept theory with example

---
+++ Passing Data (Controller to View)/PassingData(ControllerToView)_ViewData/PassingData(ControllerToView)_ViewData/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PassingData_ControllerToView__ViewData.Models;
+
+namespace PassingData_ControllerToView__ViewData.Controllers
+{    
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {   
+        [Route("")]
+        [Route("~/")]
+        [Route("[action]")]
+        public IActionResult Index()
+        {
+            ViewData["var1"] = "Hello from ViewData.";
+            ViewData["var2"] = DateTime.Now.ToLongDateString();
+
+            string[] myArray = { "Lelin", "Israt", "Jahan" };
+            ViewData["var3"] = myArray;
+            ViewData["var4"] = new List<string>()
+            {
+                "Cricket",
+                "Football",
+                "Hockey",
+                "Bolly"
+            };
+
+
+            // Use Students Class From Models
+
+            Students std1 = new Students();
+            std1.stdName = "Tanvir";
+            std1.stdGender = "Male";
+            std1.stdClass = 12;
+
+            ViewData["var5"] = std1;
+            
+            return View();
+        }
+    }
+}
---
+++ Passing Data (Controller to View)/PassingData(ControllerToView)_ViewData/PassingData(ControllerToView)_ViewData/Models/Students.cs
@@ -0,0 +1,9 @@
+﻿namespace PassingData_ControllerToView__ViewData.Models
+{
+    public class Students
+    {
+        public string stdName { get; set; }
+        public string stdGender { get; set; }
+        public int stdClass { get; set; }
+    }
+}
