4c8d8e6e8540c87fd94f3575eb4e8f8ffcac3050
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-04-30 12:12:14 +0600

    Action Methods concept theory with example

---
+++ Action Methods/ActionMethods_Demo_WebApplication/ActionMethods_Demo_WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ActionMethods_Demo_WebApplication.Controllers
+{   
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {   
+        [Route("")]
+        [Route("~/")]
+        [Route("[action]")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [Route("[action]/{msg}")]
+        public string Message(string msg)
+        {
+            return "Hello "+msg;
+        }
+
+        [Route("[action]/{id1}/{id2}")]
+        public int Id(int id1, int id2)
+        {
+            return id1 + id2;
+        }
+    }
+}
