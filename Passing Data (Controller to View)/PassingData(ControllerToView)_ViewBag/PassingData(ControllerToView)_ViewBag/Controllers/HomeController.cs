b91fac978605e29ea09a9776fe9d93f61b2c0984
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-04-30 23:05:13 +0600

    ViewBag concept completed successfuly

---
+++ Passing Data (Controller to View)/PassingData(ControllerToView)_ViewBag/PassingData(ControllerToView)_ViewBag/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PassingData_ControllerToView__ViewBag.Models;
+
+namespace PassingData_ControllerToView__ViewBag.Controllers
+{   
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {   
+        [Route("")]
+        [Route("~/")]
+        [Route("[action]")]
+        public IActionResult Index()
+        {
+            ViewBag.var1 = "Data from ViewBag";
+            ViewBag.var2 =DateTime.Now.ToLongDateString();
+
+            string[] myArray = { "Lelin", "Israt", "Jahan" };
+            ViewBag.var3 = myArray;
+
+            ViewBag.var4 = new List<string>()
+            { "I am Lelin", "My 1/3 heart name Israt", "Jahan is the second part of her name" };
+
+
+            Employees emp1 = new Employees();
+            emp1.EmpID = 1;
+            emp1.EmpName = "Lelin";
+            emp1.EmpGender = "Male";
+
+            ViewBag.var5 = emp1;    
+
+            return View();
+        }
+    }
+}
---
+++ Passing Data (Controller to View)/PassingData(ControllerToView)_ViewBag/PassingData(ControllerToView)_ViewBag/Models/Employees.cs
@@ -0,0 +1,9 @@
+﻿namespace PassingData_ControllerToView__ViewBag.Models
+{
+    public class Employees
+    {
+        public int EmpID { get; set; }
+        public string EmpName { get; set; }
+        public string EmpGender { get; set; }
+    }
+}
