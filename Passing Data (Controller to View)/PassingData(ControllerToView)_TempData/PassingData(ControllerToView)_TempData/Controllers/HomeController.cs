4cc7a7bb82c9b673abd2426191675328482e1758
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-05-01 09:25:35 +0600

    Passing data controller to view by TempData procedure completed

---
+++ Passing Data (Controller to View)/PassingData(ControllerToView)_TempData/PassingData(ControllerToView)_TempData/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PassingData_ControllerToView__TempData.Controllers
+{   
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {   
+        [Route("")]
+        [Route("~/")]
+        [Route("[action]")]
+
+        public IActionResult Index()
+        {
+            ViewData["varD1"] = "From ViewData";
+            ViewBag.varB1 = "From ViewBag";
+            TempData["varT1"] = "From TempData";
+
+            string[] myArray = { "Lelin", "Israt", "Jahan" };
+
+            TempData["varArray"] = myArray;
+
+            TempData.Keep("varArray");
+            return View();
+        }
+        [Route("[action]")]
+        public IActionResult About()
+        {
+            TempData.Keep("varT1");
+            return View();
+        }
+        [Route("[action]")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
