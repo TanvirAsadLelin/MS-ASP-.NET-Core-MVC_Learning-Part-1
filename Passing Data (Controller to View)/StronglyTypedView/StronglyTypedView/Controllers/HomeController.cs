0b8a4217ecb46ce71e0832dcfa932c53a598a8c9
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-05-01 14:21:49 +0600

    Passing data controller to view by StronglyTypedView procedure completed

---
+++ Passing Data (Controller to View)/StronglyTypedView/StronglyTypedView/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StronglyTypedView.Models;
+
+namespace StronglyTypedView.Controllers
+{    
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {    
+        [Route("")]
+        [Route("~/")]
+        [Route("[action]")]
+        public IActionResult Index()
+        {   
+
+            Employee emp1 = new Employee();
+            emp1.empName = "Lelin";
+            emp1.empGender = "Male";
+            emp1.empDesignation = "MD";
+
+            Employee emp2 = new Employee();
+            emp2.empName = "Lelin";
+            emp2.empGender = "Male";
+            emp2.empDesignation = "MD";
+            
+            
+            Employee emp3 = new Employee();
+            emp3.empName = "Lelin";
+            emp3.empGender = "Male";
+            emp3.empDesignation = "MD";  
+            
+            
+            Employee emp4 = new Employee();
+            emp4.empName = "Lelin";
+            emp4.empGender = "Male";
+            emp4.empDesignation = "MD";
+
+
+            List<Employee> empList = new List<Employee>();
+
+            empList.Add(emp1);
+            empList.Add(emp2);
+            empList.Add(emp3);
+            empList.Add(emp4);
+
+
+            return View(empList);
+        }
+    }
+}
---
+++ Passing Data (Controller to View)/StronglyTypedView/StronglyTypedView/Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace StronglyTypedView.Models
+{
+    public class Employee
+    {
+        public string empName { get; set; }
+        public string empGender { get; set; }
+        public string empDesignation { get; set; }
+    }
+}
